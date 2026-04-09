0a928e210fe6d3ac7c12397d2bd35b168bee42d4
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2026-04-09 10:09:27 -0400

    feat(ui): add base layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.module.css'
+import styles from './App.module.css'
 
 function App() {
   return (
     <>
-      
+      <header className={styles.header}>
+        <div className={`${styles.headerContent} ${styles.row}`}>
+          <h1 className={styles.headerTitle}>Jammming</h1>
+          <p>Search for songs and create your custom playlist</p>
+        </div>
+      </header>
+      <main>
+        <div className={styles.row}>
+          
+        </div>
+        <div className={styles.row}>
+
+        </div>
+      </main>
     </>
   )
 }
