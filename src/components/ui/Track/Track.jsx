dd0be61da117512f6423eab2ea02915062971c5c
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2026-04-14 19:19:02 -0400

    feat(Track): handle and print song information

--- src/components/ui/Track/Track.jsx
+++ src/components/ui/Track/Track.jsx
@@ -1,13 +1,13 @@
 import Button from '../../ui/Button/Button';
 import styles from './Track.module.css';
 
-function Track() {
+function Track(props) {
   return (
     <li className={styles.track}>
       <div className={styles.trackContent}>
-        <h3 className={styles.trackTitle}>Title song</h3>
-        <p className={styles.trackArtist}>Artist</p>
-        <p className={styles.trackAlbum}>Album</p>
+        <h3 className={styles.trackTitle}>{props.title}</h3>
+        <p className={styles.trackArtist}>{props.artist}</p>
+        <p className={styles.trackAlbum}>{props.album}</p>
       </div>
       <Button>Add</Button>
     </li>
